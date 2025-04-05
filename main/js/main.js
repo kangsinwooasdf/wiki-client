1bd5fbf65217d6ba08a5eb35cf4cfad2b412cd2d
BEOBMA <ssdssdtkfk@naver.com>
2025-04-06 03:23:54 +0900

    오류 수정

---
+++ main/js/main.js
@@ -0,0 +1,16 @@
+function goToList() {
+    window.location.href = '../index/index.html'; // 문서 목록
+}
+
+function goToEditor() {
+    window.location.href = '../editor/editor.html'; // 새 문서 작성
+}
+
+document.getElementById('search').addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        const query = e.target.value.trim();
+        if (query) {
+            window.location.href = `../index/index.html?search=${encodeURIComponent(query)}`;
+        }
+    }
+});  
